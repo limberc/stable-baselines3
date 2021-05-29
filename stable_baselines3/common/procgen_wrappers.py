456183591f63b3078f830306413f43266e218956
Zehua Cheng <chengzehua@outlook.com>
2021-05-30 01:31:23 +0800

    fix a small error when loading procgen env.

--- stable_baselines3/common/procgen_wrappers.py
+++ stable_baselines3/common/procgen_wrappers.py
@@ -6,6 +6,9 @@ from stable_baselines3.common.vec_env import VecExtractDictObs, VecMonitor, VecN
 def make_procgen_env(n_envs: int = 2,
                      env_name: str = 'starpilot'):
     assert len([key for key in gym.envs.registry.env_specs.keys() if env_name in key]), 'Not found in procgen.'
+    if 'procgen' in env_name:
+        # to note that ProcgenEnv only accept env_id style like 'starpilot'.
+        env_name = env_name[8:-3]
     venv = ProcgenEnv(num_envs=n_envs, env_name=env_name)
     venv = VecExtractDictObs(venv, 'rgb')
     venv = VecMonitor(venv=venv)
