3ad113def1ebd6a5e3d0117f31df7f206c41abd0
Zehua Cheng <chengzehua@outlook.com>
2021-05-29 21:23:56 +0800

    Add VecNormalize.

--- stable_baselines3/common/procgen_wrappers.py
+++ stable_baselines3/common/procgen_wrappers.py
@@ -1,11 +1,13 @@
 import gym
 from procgen import ProcgenEnv
 
-from stable_baselines3.common.vec_env import VecMonitor
+from stable_baselines3.common.vec_env import VecMonitor, VecNormalize
 
 
 def procgen_wrapper(num_envs: int = 2,
                     env_name: str = 'starpilot'):
     assert len([key for key in gym.envs.registry.env_specs.keys() if env_name in key]), 'Not found in procgen.'
     venv = ProcgenEnv(num_envs=num_envs, env_name=env_name)
-    return VecMonitor(venv=venv)
+    venv = VecMonitor(venv=venv)
+    venv = VecNormalize(venv=venv, ob=False)
+    return venv
