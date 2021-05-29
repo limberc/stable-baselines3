4f128e430da4cd9199a2b0c46ad2fd006dcbb08b
Zehua Cheng <chengzehua@outlook.com>
2021-05-30 01:04:55 +0800

    reformat procgen_wrapper -> make_procgen_env.

--- stable_baselines3/common/procgen_wrappers.py
+++ stable_baselines3/common/procgen_wrappers.py
@@ -1,13 +1,13 @@
 import gym
 from procgen import ProcgenEnv
+from stable_baselines3.common.vec_env import VecExtractDictObs, VecMonitor, VecNormalize
 
-from stable_baselines3.common.vec_env import VecMonitor, VecNormalize
 
-
-def procgen_wrapper(num_envs: int = 2,
-                    env_name: str = 'starpilot'):
+def make_procgen_env(n_envs: int = 2,
+                     env_name: str = 'starpilot'):
     assert len([key for key in gym.envs.registry.env_specs.keys() if env_name in key]), 'Not found in procgen.'
-    venv = ProcgenEnv(num_envs=num_envs, env_name=env_name)
+    venv = ProcgenEnv(num_envs=n_envs, env_name=env_name)
+    venv = VecExtractDictObs(venv, 'rgb')
     venv = VecMonitor(venv=venv)
-    venv = VecNormalize(venv=venv, ob=False)
+    venv = VecNormalize(venv=venv, norm_obs=False)
     return venv
